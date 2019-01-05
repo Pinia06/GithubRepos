0f377a0bd992844952cd9159a7811b3fdc18f869
--unset <--unset>
2019-01-05 22:52:06 +0000

    UI updated

--- app/src/main/java/com/example/anas/gitrepos/MainActivity.java
+++ app/src/main/java/com/example/anas/gitrepos/MainActivity.java
@@ -6,13 +6,18 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 //This Activity's role is to choose the proper fragment to be displayed
 
 public class MainActivity extends AppCompatActivity {
 
     private final String DEBUG_TAG = "MainActivityTAG";
+    private final String TRENDING_FRAGMENT = "Trending Repositories";
+    private final String SETTINGS_FRAGMENT = "App Settings";
+
     private BottomNavigationView mBottomNavigation;
+    private TextView titleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +25,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mBottomNavigation = findViewById(R.id.bottom_navigation);
+        titleTextView = findViewById(R.id.tv_title);
 
         //Setting Trending Fragment as the default UI on MainActivity Creation
         getSupportFragmentManager().beginTransaction().replace(R.id.fl_fragment_container,new TrendingFragment()).commit();
+        titleTextView.setText(TRENDING_FRAGMENT);
     }
 
     @Override
@@ -44,10 +51,12 @@ public class MainActivity extends AppCompatActivity {
                 switch(itemSelected){
                     //Case Trending is the selected option
                     case R.id.nav_trending : selectedFragment = new TrendingFragment();
+                                             titleTextView.setText(TRENDING_FRAGMENT);
                         break;
 
                     //Case Settings is the selected option
                     case R.id.nav_settings : selectedFragment = new SettingsFragment();
+                                             titleTextView.setText(SETTINGS_FRAGMENT);
                         break;
                 }
 
